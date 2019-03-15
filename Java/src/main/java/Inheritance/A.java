3fa70dd0d59af2a7cd9daacd34f430644933ee33
Avi Gupta <gupta.av@HCL.COM>
2019-03-15 16:33:27 +0530

    Java topics code

---
+++ Java/src/main/java/Inheritance/A.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class A {
+	
+	
+	public void Show() {
+		
+		System.out.println("Parent class of show method");
+	}
+	
+	
+	public void Mirror() {
+		
+		System.out.println("Parent class of Mirror method");
+	}
+
+}
