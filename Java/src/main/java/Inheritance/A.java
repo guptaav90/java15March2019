51f74db87e5a244bf57e298db65620fc97f20a1e
guptaav90 <48586644+guptaav90@users.noreply.github.com>
2019-03-15 16:42:41 +0530

    Java topics updated code
    Java topics updated code

--- Java/src/main/java/Inheritance/A.java
+++ Java/src/main/java/Inheritance/A.java
@@ -5,13 +5,13 @@ public class A {
 	
 	public void Show() {
 		
-		System.out.println("Parent class of show method");
+		System.out.println("Parent class of SHOW method");
 	}
 	
 	
 	public void Mirror() {
 		
-		System.out.println("Parent class of Mirror method");
+		System.out.println("Parent class of MIRROR method");
 	}
 
 }
